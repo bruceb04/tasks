eb48b4f4ecef61085e3ba1996600398866a28259
Bruce Bermel <brucebermel14@gmail.com>
2024-08-30 13:07:59 -0400

    Added name and changed color

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript by Bruce Bermel
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <p>Green background too!</p>
         </div>
     );
 }
