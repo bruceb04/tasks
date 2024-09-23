1fbb7ebbcbbb3f17a5a7e1a9813bf133979e105c
Bruce Bermel <brbe@udel.edu>
2024-09-23 12:41:50 -0400

    The merge issues are really bad

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+let listStuff = ["Item 1", "Item 2", "Item 3"];
 
 function App(): React.JSX.Element {
     return (
@@ -19,10 +22,35 @@ function App(): React.JSX.Element {
                     ))}
                 </ul>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <Container>
+                <Row>
+                    <Col
+                        style={{
+                            width: "50%",
+                            height: "50%",
+                            backgroundColor: "red",
+                        }}
+                    >
+                        Column 1
+                    </Col>
+                    <Col
+                        style={{
+                            width: "50%",
+                            height: "50%",
+                            backgroundColor: "red",
+                        }}
+                    >
+                        Column 2
+                    </Col>
+                </Row>
+            </Container>
+            <h1>
+                <img
+                    src="https://cdn.discordapp.com/attachments/1197987199371718768/1223349837831868560/IMG_7579.jpg?ex=66da0352&is=66d8b1d2&hm=4c0bc7c8c68952f0635f30868e16bc868233c563178bfba50737aa9ce3d48a71&"
+                    alt="A breathtaking performance"
+                />
+            </h1>
+            <p>Green background too! Hello World!</p>
         </div>
     );
 }
