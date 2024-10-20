7bcf50e6a56a3b09b694576a05026df90e8e2863
Bruce Bermel <brbe@udel.edu>
2024-10-20 19:06:28 -0400

    Fixed imports again

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-export function MultipleChoiceQuestion({}: {
+export function MultipleChoiceQuestion({
+    options,
+    expectedAnswer,
+}: {
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [bruh, setBruh] = useState("second");
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <h3>{bruh}</h3>
+            <h3>{options}</h3>
+            <h3>{expectedAnswer}</h3>
         </div>
     );
 }
